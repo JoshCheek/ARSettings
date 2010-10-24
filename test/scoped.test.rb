8b3f38d5090d5214081ce356e15c2f982de29132
Josh Cheek <josh.cheek@gmail.com>
2010-10-23 21:44:35 -0500

    bring back the scoped tests

--- test/scoped.test.rb
+++ test/scoped.test.rb
@@ -205,6 +205,8 @@ class TestScoping < Test::Unit::TestCase
       assert_nothing_raised { Setting.scope :Symbol  }
       assert_nothing_raised { Setting.scope Class    }
       assert_raises(ARSettings::InvalidScopeError) { Setting.scope 1 }
+      assert_raises(ARSettings::InvalidScopeError) { Setting.scope Object.new }
+      assert_raises(ARSettings::InvalidScopeError) { Setting.scope(SomeConstant=Object.new) }
     end
     
     verify 'can add a scope as an option' do
@@ -223,49 +225,34 @@ class TestScoping < Test::Unit::TestCase
       assert_equal Setting::Scoped , Setting.scope(String).class
     end
   
-  # verify 'scoped knows its settings class' do
-  #   assert_equal Setting , Setting.scope(String).settings_class
-  # end
-  # 
-  # verify 'current_scope returns the current scope' do
-  #   assert_equal :String , Setting.scope(String).current_scope
-  #   assert_equal :string , Setting.scope('string').current_scope
-  # end
-  # 
-  # verify 'default scope is the the symbol of the settings class itself' do
-  #   assert_equal :Setting , Setting.current_scope
-  # end
-  # 
-  # verify 'can use strings or symbols for scope as well' do
-  #   assert_equal :abc , Setting.scope(:abc).current_scope
-  #   assert_equal :abc , Setting.scope('abc').current_scope
-  # end
-  # 
-  # verify 'raises error for non symbol/string/class' do
-  #   assert_nothing_raised { Setting.scope("abc")            }
-  #   assert_nothing_raised { Setting.scope(:abc)             }
-  #   assert_nothing_raised { Setting.scope(Abc = Class.new)  }
-  #   assert_raises(Setting::InvalidScopeError) { Setting.scope(1) }
-  # end
-  # 
-  # verify 'can add and remove variables within a given scope' do
-  #   Setting.add_setting :abcd , :scope => String , :default => 12
-  #   string_settings = Setting.scope(String)
-  #   assert_equal 12 , string_settings.abcd
-  #   string_settings.abcd = 5
-  #   assert_equal 5 , string_settings.abcd
-  # end
-  # 
-  # verify 'raises NoSuchSettingError when given incorrect scope' do
-  #   Setting.add_setting :abcd , :scope => String , :default => 12
-  #   assert_raises(Setting::NoSuchSettingError) { Setting.abcd }
-  #   assert_raises(Setting::NoSuchSettingError) { Setting.abcd = 3 }
-  #   assert_nothing_raised { Setting.scope(String).abcd }
-  #   assert_nothing_raised { Setting.scope(String).abcd = 5 }
-  # end
-  # 
-  # verify 'scoped settings can list all settings and values'
-  # verify 'reset only applies to settings of a given scope'
+    verify 'scoped knows its settings class' do
+      assert_equal Setting , Setting.scope(String).settings_class
+    end
+  
+    verify 'current_scope returns the current scope' do
+      assert_equal :String , Setting.scope(String).scope
+      assert_equal :string , Setting.scope('string').scope
+      assert_equal :strIng , Setting.scope(:strIng).scope
+    end
+    
+  
+    verify 'can add and remove variables within a given scope' do
+      s.add_setting :abcd , :default => 12
+      assert_equal 12 , s.abcd
+      s.abcd = 5
+      assert_equal 5 , s.abcd
+    end
+  
+    verify 'raises NoSuchSettingError when given incorrect scope' do
+      s.add_setting :abcd , :default => 12
+      assert_raises(Setting::NoSuchSettingError) { Setting.abcd }
+      assert_raises(Setting::NoSuchSettingError) { Setting.abcd = 3 }
+      assert_nothing_raised { s.abcd }
+      assert_nothing_raised { s.abcd = 5 }
+    end
+  
+    verify 'scoped settings can list all settings and values'
+    verify 'reset only applies to settings of a given scope'
   end
   
   
