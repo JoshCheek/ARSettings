e700a8f070babf79d4c4e1ea157f5eb6f9b1dd74
Josh Cheek <josh.cheek@gmail.com>
2010-11-15 12:33:41 -0600

    move activerecord code into arsettings.rb to avoid violating packaging standard

--- lib/arsettings.rb
+++ lib/arsettings.rb
@@ -5,10 +5,25 @@ manifest = %w(
   settings_class/class_methods
   settings_class/instance_methods
   packaged
-  activerecord
   has_settings
 )
 
 manifest.each do |filename|
   require File.dirname(__FILE__) << "/arsettings/#{filename}"
+end
+
+
+
+# placing this here because otherwise it violates packaging standard
+class ActiveRecord::Base
+  
+  def self.inherited_with_settings(subclass)
+    inherited_without_settings subclass
+    subclass.extend ARSettings::HasSettings
+  end
+  
+  class << self
+    alias_method_chain :inherited, :settings
+  end
+  
 end
