9c0fe7c972f7a991e80204166d8e172372ae4e87
Josh Cheek <josh.cheek@gmail.com>
2010-11-14 22:46:40 -0600

    made the code to add has_setting to ARB classes nicer

--- lib/arsettings/activerecord.rb
+++ lib/arsettings/activerecord.rb
@@ -1,9 +1,12 @@
 class ActiveRecord::Base
   
   old_inherited = method :inherited
-  (class << self; self; end).send :define_method , :inherited do |subclass|
-    old_inherited.call(subclass)
-    (class << subclass ; self ; end).send :include , ARSettings::HasSettings
+  
+  (class << self ; self ; end).instance_eval do
+    define_method :inherited do |subclass|
+      old_inherited.call subclass
+      subclass.extend ARSettings::HasSettings
+    end
   end
   
 end
