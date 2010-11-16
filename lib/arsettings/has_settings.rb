b2ee56870cae21f94af72cde60e48fbeb6244d7f
Josh Cheek <josh.cheek@gmail.com>
2010-11-16 04:25:44 -0600

    made has_setting a little simpler after reek complained

--- lib/arsettings/has_settings.rb
+++ lib/arsettings/has_settings.rb
@@ -6,19 +6,13 @@ module ARSettings
       instance = inner_options.delete :instance
       package = @arsettings_package
       package.add name , inner_options , &block
-      getter = name
-      setter = "#{name}="
-      boolean_getter = "#{name}?"
-      (class << self ; self ; end).instance_eval do
-        define_method getter          do       package.send getter          end
-        define_method setter          do |arg| package.send setter , arg    end
-        define_method boolean_getter  do       package.send boolean_getter  end
+      definitions = lambda do |*args|
+        define_method name        do       package.send name              end # getter
+        define_method "#{name}="  do |arg| package.send "#{name}=" , arg  end # setter
+        define_method "#{name}?"  do       package.send "#{name}?"        end # boolean-getter
       end
-      if instance
-        define_method getter          do       package.send getter          end
-        define_method boolean_getter  do       package.send boolean_getter  end
-        define_method setter          do |arg| package.send setter , arg    end
-      end
+      (class << self ; self ; end).instance_eval &definitions
+      definitions.call if instance
     end
     
   end
